57faaf7765e63c356ebbeac5e93d3dfe89ce7844
Sven van Hal <S.R.P.vanHal@student.tudelft.nl>
2014-10-22 19:31:08 +0200

    Fix nullpointers on maze boundaries.

--- src/ants/Ant.java
+++ src/ants/Ant.java
@@ -21,7 +21,7 @@ public class Ant {
 		brain = new Explorer();
 		maxAge = newMaxAge;
 		pheromoneAmount = newPheromoneAmount;
-		
+
 		path = new ArrayList<Node>();
 	}
 
@@ -29,7 +29,7 @@ public class Ant {
 		brain = newBrain;
 		maxAge = newMaxAge;
 		pheromoneAmount = newPheromoneAmount;
-		
+
 		path = new ArrayList<Node>();
 	}
 
@@ -42,6 +42,7 @@ public class Ant {
 
 	/**
 	 * A setter for location
+	 * 
 	 * @param newLocation
 	 */
 	public void setLocation(int[] newLocation) {
@@ -50,6 +51,7 @@ public class Ant {
 
 	/**
 	 * A setter based on a node as argument
+	 * 
 	 * @param newNode
 	 */
 	public void moveTo(Node newNode) {
@@ -58,6 +60,7 @@ public class Ant {
 
 	/**
 	 * The running tactics of the Ant.
+	 * 
 	 * @param maze
 	 * @param startLocation
 	 * @param goalLocation
@@ -66,88 +69,98 @@ public class Ant {
 		location = startLocation;
 		path.add(maze.getNode(location));
 		ArrayList<Node> toGo = new ArrayList<Node>();
-		
-		
-		//First step - without predecessor
+
+		// First step - without predecessor
 		toGo = maze.getNode(location).getNeighbours();
 		path.add(brain.decide(toGo));
-		
-		for(int i = 0; i < maxAge; i++){
+
+		for (int i = 0; i < maxAge; i++) {
 			// CONS
-			System.out.println("[Ant|run] Location: " + location[0] + " " + location[1]);			
-			
+			System.out.println("[Ant|run] Location: " + location[0] + " " + location[1]);
+
 			// Goal reached?
-			if(Arrays.equals(location,goalLocation)){
+			if (Arrays.equals(location, goalLocation)) {
 				System.out.println("[Ant|run] Reached goal");
 				spreadPheromone();
 				return;
 			}
-			
+
 			// Collect neighbours where I can go.
 			toGo = selectNeighbours();
-			
+
 			// Brain decide, makes behaviour.
-			location = brain.decide(toGo).getLocation();
-			path.add(maze.getNode(location));
+			Node nextNode = brain.decide(toGo);
+
+			if (nextNode != null) {
+
+				location = nextNode.getLocation();
+				path.add(maze.getNode(location));
+
+			} else {
+				System.out.println("Can't find a next node from location " + location + " (previous path: " + path + ")");
+			}
 		}
 		System.out.println("This ant died of old age.");
 	}
 
-	private ArrayList<Node> selectNeighbours(){
+	private ArrayList<Node> selectNeighbours() {
 		// The neighbours of current locations
-		ArrayList<Node> list = path.get(path.size()-1).getNeighbours();
-		
-		if(accessibleNeighbours(list)<2){
+		ArrayList<Node> list = path.get(path.size() - 1).getNeighbours();
+
+		if (accessibleNeighbours(list) < 2) {
 			backOff();
 		}
-		
+
 		list.clear();
-		list = path.get(path.size()-1).getNeighbours();
+		list = path.get(path.size() - 1).getNeighbours();
 		// We do not want to move back to where we came from.
-		list.remove(path.get(path.size()-2));
+		list.remove(path.get(path.size() - 2));
 		return list;
 	}
+
 	/**
-	 * Add pheromone to node based on path. 
-	 * Currently adds uniquely to nodes in reversed quadratic comparison.
-	 * Aka: 1/x^2.
+	 * Add pheromone to node based on path. Currently adds uniquely to nodes in reversed quadratic comparison. Aka: 1/x^2.
 	 */
-	private void spreadPheromone(){
-		float newPheromones = pheromoneAmount/(path.size()^2);
+	private void spreadPheromone() {
+		float newPheromones = pheromoneAmount / (path.size() ^ 2);
 		ArrayList<Node> route = new ArrayList<Node>();
-		
+
 		// Add to route unique from path
-		for(int i = 0; i < path.size()-1; i ++){
-			if(!route.contains(path.get(i))){
+		for (int i = 0; i < path.size() - 1; i++) {
+			if (!route.contains(path.get(i))) {
 				route.add(path.get(i));
 			}
 		}
-		
-		for(int i = 0; i < route.size()-1; i ++){
+
+		for (int i = 0; i < route.size() - 1; i++) {
 			route.get(i).updatePheromoneLevel(newPheromones);
 		}
-		
-		System.out.println("Reached the end in "+path.size()+" steps.");
-		System.out.println("Releasing "+newPheromones+" onto the path.");
+
+		System.out.println("Reached the end in " + path.size() + " steps.");
+		System.out.println("Releasing " + newPheromones + " onto the path.");
 	}
-	
-	// If we find a dead end. 
-	private void backOff(){
+
+	// If we find a dead end.
+	private void backOff() {
 		int size = path.size();
-		for(int i = size-1;(i >= 0)&&(accessibleNeighbours( path.get(i).getNeighbours() ) < 3) ; i--){
+		for (int i = size - 1; (i >= 0) && (accessibleNeighbours(path.get(i).getNeighbours()) < 3); i--) {
 			// Step back is there are less then three accesible neighbours.
-			int [] loc = path.get(i).getLocation();
-			System.out.println("[Ant|backOff] Removing: "+loc[0]+"x" + loc[1]); 
+			int[] loc = path.get(i).getLocation();
+			System.out.println("[Ant|backOff] Removing: " + loc[0] + "x" + loc[1]);
 			path.remove(i);
 		}
 		// you are now on the old crossroad.
 	}
 
-	private int accessibleNeighbours(ArrayList<Node> neighbours){
+	private int accessibleNeighbours(ArrayList<Node> neighbours) {
 		// How many of the given nodes are accessible
 		int count = 0;
-		for(int i = 0; i < neighbours.size(); i++){
-			count += neighbours.get(i).isAccessible()?1:0;
+		for (int i = 0; i < neighbours.size(); i++) {
+
+			if (neighbours.get(i) == null)
+				continue;
+
+			count += neighbours.get(i).isAccessible() ? 1 : 0;
 		}
 		return count;
 	}
--- src/ants/Follower.java
+++ src/ants/Follower.java
@@ -8,31 +8,34 @@ import node.Node;
 public class Follower implements Brain {
 
 	/**
-	 * This decides on which node to to go
-	 * The Follower Brain takes the path with the most pheromone
-	 * @throws Exception 
+	 * This decides on which node to to go The Follower Brain takes the path with the most pheromone
+	 * 
+	 * @throws Exception
 	 */
-	public Node decide(ArrayList<Node> list){
-		
+	public Node decide(ArrayList<Node> list) {
+
 		Random random = new Random();
-		
+
 		double sum = 0;
 		// Check for every neighbouring cell
-		for(int i = 0;i<list.size();i++){
+		for (int i = 0; i < list.size(); i++) {
+			
+			if(list.get(i) == null)
+				continue;
+			
 			sum = sum + list.get(i).getPheromoneLevel();
 		}
-		System.out.println("Out of "+sum);
+		System.out.println("Out of " + sum);
 		// We want 0 exclusive and 1 inclusive
 		// Thats why we do 1-random. It doesn't allow zero.
-		double chosen = (1-random.nextDouble())*sum;
-		System.out.println("Look for "+chosen+"\n");
-		
-		
+		double chosen = (1 - random.nextDouble()) * sum;
+		System.out.println("Look for " + chosen + "\n");
+
 		double current = 0;
 		int node = 0;
-		for(int i = 0;i < list.size(); i++){
-			if(chosen > current){
-				System.out.println("Between 0 and "+current);
+		for (int i = 0; i < list.size(); i++) {
+			if (chosen > current) {
+				System.out.println("Between 0 and " + current);
 				current = list.get(i).getPheromoneLevel();
 				chosen = chosen - current;
 			} else {
@@ -40,7 +43,7 @@ public class Follower implements Brain {
 				break;
 			}
 		}
-		//System.out.println("Chosen the "+node);
+		// System.out.println("Chosen the "+node);
 		return list.get(node);
 	}
 }
